22b469ff2d378bebb0c113af00a3ed5e6b68e6e8
MauricioHS91 <mauriciohansen@unipam.edu.br>
2024-11-04 18:29:44 -0300

    version 3.0
    versão 3.0

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+include "template/header.php";
+include "db.php"; // Arquivo que faz a conexão com o banco de dados
+
+$query = "SELECT * FROM produtores";
+$result = mysqli_query($conn, $query);
+$produtores = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -16,213 +25,27 @@
     </style>
 </head>
 <body>
-        
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Feira de Produtores</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Início</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Barracas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Sobre</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contato</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    
     <div class="container mt-5">
         <h1 class="text-center mb-4">Feira de Produtores</h1>
         <div class="row row-cols-1 row-cols-md-3 g-4">
-            <!-- Card 1 -->
-            <div class="col">
-                <div class="card h-100">
-                    <a href="produtor1.php">
-                        <img src="img/produtor2.jpg" class="card-img-top" alt="Barraca 1">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 1</h5>
-                        <p class="card-text">Descrição da barraca 1.</p>
+            <?php foreach ($produtores as $produtor): ?>
+                <div class="col">
+                    <div class="card h-100">
+                        <a href="produtor.php?produtor_id=<?php echo $produtor['id']; ?>">
+                            <img src="<?php echo $produtor['imagem']; ?>" class="card-img-top" alt="<?php echo $produtor['nome']; ?>">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $produtor['nome']; ?></h5>
+                            <p class="card-text"><?php echo $produtor['descricao']; ?></p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- Card 2 -->
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="img/FEIRA.jpg" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="img/PRODUTOR1.jpg" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="produtor1.php">
-                        <img src="img/produtor6.jpg" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="img/produtor3.jpg" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="img/produtor4.jpg" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="img/produtor5.jpg" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="img/produtor6.jpg" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <a href="URL_DO_PRODUTOR_2">
-                        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Barraca 2">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Barraca 2</h5>
-                        <p class="card-text">Descrição da barraca 2.</p>
-                    </div>
-                </div>
-            </div>
-            <!-- Repita para os demais cards, alterando os URLs -->
+            <?php endforeach; ?>
         </div>
     </div>
-
-    
-    <footer class="bg-light text-center text-lg-start mt-5">
-        <div class="container p-4">
-            <p class="text-center">© 2024 Feira de Produtores - Todos os direitos reservados</p>
-        </div>
-    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
+<?php include "template/footer.php"; ?>
---
+++ template/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <script type="text/javascript" src="/js/script.js"></script>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TG-04013</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Feira de Produtores</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Barracas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sobre.php">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contato.php">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
