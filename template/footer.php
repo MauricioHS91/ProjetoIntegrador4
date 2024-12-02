66b329e2162b8a62a36645135340d06191a7e36a
MauricioHS91 <mauriciohansen@unipam.edu.br>
2024-12-02 17:20:20 -0300

    Merge branch 'main' of https://github.com/MauricioHS91/ProjetoIntegrador4

--- template/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-light text-center text-lg-start mt-5">
-        <div class="container p-4">
-            <p class="text-center">© 2024 Feira de Produtores - Todos os direitos reservados</p>
-        </div>
-    </footer>
